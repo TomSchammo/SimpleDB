3d80f7f53abb50b25145fd235032dad2dcc9c273
Tom Schammo <tom@tomschammo.com>
2022-05-05 21:28:17 +0200

    feat(REPL): Add simple REPL
    Add a simple REPL that only recognizes the `.exit` command for now.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::io::{stdin, stdout, Write};
+
+const EXIT_SUCCESS: i32 = 0;
+
+fn read_input() -> String {
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).expect("Invalid input!");
+
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+
+    // windows uses \r\n for new lines whereas unix only uses \n
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    input
+}
+
+fn print_prompt() {
+    print!("db > ");
+    let _ = stdout().flush();
+}
+
+fn match_command(command: &str) {
+    match command {
+        ".exit" => {
+            std::process::exit(EXIT_SUCCESS);
+        }
+        _ => {
+            println!("Unrecognized command: '{command}'.");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_prompt();
+        let input = read_input();
+        match_command(&input);
+    }
 }
