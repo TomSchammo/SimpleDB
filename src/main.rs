5aa31a6740271e807172232f6dad23c9d6ca46f3
Tom Schammo <tom@tomschammo.com>
2022-05-05 21:39:25 +0200

    refactor(REPL): Move REPL code into its own file
    Move REPL code into its own file and provide a public interface to
    run it.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,5 @@
-use std::io::{stdin, stdout, Write};
-
-const EXIT_SUCCESS: i32 = 0;
-
-fn read_input() -> String {
-    let mut input = String::new();
-
-    stdin().read_line(&mut input).expect("Invalid input!");
-
-    if let Some('\n') = input.chars().next_back() {
-        input.pop();
-    }
-
-    // windows uses \r\n for new lines whereas unix only uses \n
-    if let Some('\r') = input.chars().next_back() {
-        input.pop();
-    }
-
-    input
-}
-
-fn print_prompt() {
-    print!("db > ");
-    let _ = stdout().flush();
-}
-
-fn parse_command(command: &str) {
-    match command {
-        ".exit" => {
-            std::process::exit(EXIT_SUCCESS);
-        }
-        _ => {
-            println!("Unrecognized command: '{command}'.");
-        }
-    }
-}
+mod repl;
 
 fn main() {
-    loop {
-        print_prompt();
-        let input = read_input();
-        parse_command(&input);
-    }
+    repl::run();
 }
---
+++ src/repl.rs
@@ -0,0 +1,44 @@
+use std::io::{stdin, stdout, Write};
+
+const EXIT_SUCCESS: i32 = 0;
+
+fn read_input() -> String {
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).expect("Invalid input!");
+
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+
+    // windows uses \r\n for new lines whereas unix only uses \n
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    input
+}
+
+fn print_prompt() {
+    print!("db > ");
+    let _ = stdout().flush();
+}
+
+fn parse_command(command: &str) {
+    match command {
+        ".exit" => {
+            std::process::exit(EXIT_SUCCESS);
+        }
+        _ => {
+            println!("Unrecognized command: '{command}'.");
+        }
+    }
+}
+
+pub fn run() {
+    loop {
+        print_prompt();
+        let input = read_input();
+        parse_command(&input);
+    }
+}
